2d765ee6f5d5b825ff61e8db075f19deeee9b5d6
Carlos Francisco Herrera Franco <francisco.herrera@proan.com>
2024-04-24 14:36:56 -0600

    fix vercel deploy unused vars

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { ConfigureSectionDOM } from "./classes/configureSection";
 import { CreateDom } from "./classes/createDOM";
 
-let domBuilder: CreateDom;
-let configBuilder: ConfigureSectionDOM;
 function Main() {
-  domBuilder = new CreateDom();
-  configBuilder = new ConfigureSectionDOM();
+  const domBuilder = new CreateDom();
+  const configBuilder = new ConfigureSectionDOM();
 }
 
 document.addEventListener("DOMContentLoaded", Main);
